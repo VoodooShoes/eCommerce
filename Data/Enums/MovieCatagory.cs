c4e8f6b52ea21a0cd0122fff025f01d110b2debb
Spencer Hughes <voodooshoes6430@gmail.com>
2022-08-07 14:52:28 -0500

    Eliminated spaces in Sci Fi and Comic Book,

--- Data/Enums/MovieCatagory.cs
+++ Data/Enums/MovieCatagory.cs
@@ -7,14 +7,14 @@ namespace eCommerce.Data.Enums
         Action = 1,
         Animation = 2,
         Comedy = 3,
-        Comic Book = 4,
+        ComicBook = 4,
         Drama = 5,
         Educational = 6,
         Family = 7,
         Fantasy = 8,
         Horror = 9,
         Romance = 10,
-        Sci Fi = 11,
+        SciFi = 11,
         Sports = 12,
         Western = 13
 
