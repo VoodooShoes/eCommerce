669a71e391ec40ad825dc8f9ed364675f5b834f0
Spencer Hughes <voodooshoes6430@gmail.com>
2022-08-15 18:53:53 -0500

    Added IActorService Interface and services folder under data.

---
+++ Data/Services/IActorService.cs
@@ -0,0 +1,15 @@
+﻿using eCommerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.Data.Services
+{
+	public interface IActorService
+	{
+		IEnumerable<ActorModel> GetAll();
+		ActorModel GetById(int id);
+		void Add(ActorModel actor);
+
+		ActorModel Update(int id, ActorModel newActor);
+		void Delete(int id);
+	}
+}
