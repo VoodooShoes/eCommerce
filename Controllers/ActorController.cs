b45cc0f1f8de7930be558e5d0001f5956dfb9249
Spencer Hughes <voodooshoes6430@gmail.com>
2022-08-19 10:52:32 -0500

    Added create view under Actor.

--- Controllers/ActorController.cs
+++ Controllers/ActorController.cs
@@ -18,5 +18,10 @@ namespace eCommerce.Controllers
             var data = await _service.GetAll();
             return View(data);
         }
+        //Get: Actors/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
